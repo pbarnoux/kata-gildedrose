cce77fec6f792ddcad3cbcde609ef16d9ea3b2f9
Pierre Barnoux <pbarnoux@gmail.com>
2013-10-08 23:07:31 +0200

    Change all tests so that they share the same pattern: if (name is ...)
    Inverting conditions if (! ...)
      * Set cursor on the if and CTRL-1 to Quick Fix 'Invert condition'
      * Note that you must have a else block to view this option
    Splitting conditions if (A || B), if (A && B)
      * Set cursor on the logical operator, hit CTRL-1, 'Split condition'
    Grouping else { if (...) } to a single statement else if (...) {
      * Set cursor on the else keyword, hit CTRL-1, 'Change else block to statement'
    Exchanging embedded if statements
      * Set cursor on one of the if keyword, hit CTRL-1, 'Exchance conditions for inner and outer if...'
      * Note that the above fix is only shown for simple if () statements without else block

--- src/main/java/code/kata/gildedrose/Inn.java
+++ src/main/java/code/kata/gildedrose/Inn.java
@@ -23,13 +23,7 @@ public class Inn {
 
 	public void updateQuality() {
 		for (Item item : items) {
-			if (!item.getName().equals("Aged Brie") && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (item.getQuality() > 0) {
-					if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-						item.setQuality(item.getQuality() - 1);
-					}
-				}
-			} else {
+			if (item.getName().equals("Aged Brie")) {
 				if (item.getQuality() < 50) {
 					item.setQuality(item.getQuality() + 1);
 
@@ -47,26 +41,47 @@ public class Inn {
 						}
 					}
 				}
+			} else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+				if (item.getQuality() < 50) {
+					item.setQuality(item.getQuality() + 1);
+
+					if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+						if (item.getSellIn() < 11) {
+							if (item.getQuality() < 50) {
+								item.setQuality(item.getQuality() + 1);
+							}
+						}
+
+						if (item.getSellIn() < 6) {
+							if (item.getQuality() < 50) {
+								item.setQuality(item.getQuality() + 1);
+							}
+						}
+					}
+				}
+			} else if (item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+			} else {
+				if (item.getQuality() > 0) {
+					item.setQuality(item.getQuality() - 1);
+				}
 			}
 
-			if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+			if (item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+			} else {
 				item.setSellIn(item.getSellIn() - 1);
 			}
 
 			if (item.getSellIn() < 0) {
-				if (!item.getName().equals("Aged Brie")) {
-					if (!item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (item.getQuality() > 0) {
-							if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-								item.setQuality(item.getQuality() - 1);
-							}
-						}
-					} else {
-						item.setQuality(item.getQuality() - item.getQuality());
-					}
-				} else {
+				if (item.getName().equals("Aged Brie")) {
 					if (item.getQuality() < 50) {
 						item.setQuality(item.getQuality() + 1);
+					}
+				} else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+					item.setQuality(item.getQuality() - item.getQuality());
+				} else if (item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+				} else {
+					if (item.getQuality() > 0) {
+						item.setQuality(item.getQuality() - 1);
 					}
 				}
 			}
