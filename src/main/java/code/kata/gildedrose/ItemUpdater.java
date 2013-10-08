896e07a5604695e66391c5fc9812a7298c2d42c2
Pierre Barnoux <pbarnoux@gmail.com>
2013-10-09 00:08:01 +0200

    Move duplicated code on top of each if branches
    There is some issue with Backstage pass:
      * The scope of the first guard protecting the quality update can be reduced
      * When moving the sellIn decrease instruction, we break some code since sellIn is read
      * Compensate read operations done after the decrease operation and the code is fine again

--- src/main/java/code/kata/gildedrose/ItemUpdater.java
+++ src/main/java/code/kata/gildedrose/ItemUpdater.java
@@ -13,40 +13,40 @@ public class ItemUpdater {
 		}
 
 		if (item.getName().equals("Aged Brie")) {
+			item.setSellIn(item.getSellIn() - 1);
 			if (item.getQuality() < 50) {
 				item.setQuality(item.getQuality() + 1);
 			}
-			item.setSellIn(item.getSellIn() - 1);
 			if (item.getSellIn() < 0) {
 				if (item.getQuality() < 50) {
 					item.setQuality(item.getQuality() + 1);
 				}
 			}
 		} else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+			item.setSellIn(item.getSellIn() - 1);
 			if (item.getQuality() < 50) {
 				item.setQuality(item.getQuality() + 1);
+			}
 
-				if (item.getSellIn() < 11) {
-					if (item.getQuality() < 50) {
-						item.setQuality(item.getQuality() + 1);
-					}
-				}
-
-				if (item.getSellIn() < 6) {
-					if (item.getQuality() < 50) {
-						item.setQuality(item.getQuality() + 1);
-					}
+			if (item.getSellIn() +1 < 11) {
+				if (item.getQuality() < 50) {
+					item.setQuality(item.getQuality() + 1);
 				}
 			}
-			item.setSellIn(item.getSellIn() - 1);
+
+			if (item.getSellIn() +1 < 6) {
+				if (item.getQuality() < 50) {
+					item.setQuality(item.getQuality() + 1);
+				}
+			}
 			if (item.getSellIn() < 0) {
 				item.setQuality(item.getQuality() - item.getQuality());
 			}
 		} else {
+			item.setSellIn(item.getSellIn() - 1);
 			if (item.getQuality() > 0) {
 				item.setQuality(item.getQuality() - 1);
 			}
-			item.setSellIn(item.getSellIn() - 1);
 			if (item.getSellIn() < 0) {
 				if (item.getQuality() > 0) {
 					item.setQuality(item.getQuality() - 1);
