29404a2d0858b1ea7dede20cbab31148874ebeb9
Pierre Barnoux <pbarnoux@gmail.com>
2013-10-08 23:18:52 +0200

    Extract update logic in a dedicated class
    Extract the loop scope to a private method
      * ALT-SHIFT-LEFT_ARROW to select all the block scope
      * ALT-SHIFT-M to move the selected code to a new method
      * CTRL-1 on the loop after extraction to add braces
      * CTRL-1 on the extracted method to remove useless block braces
    Add an object parameter (required to move the extracted method to another class)
      * ALT-SHIFT-T to bring the refactoring menu while cursor is on the method name
      * Introduce parameter object
      * ALT-SHIFT-V to move the method on that class
      * ALT-SHIFT-C to remove useless parameter added when moving the method

---
+++ src/main/java/code/kata/gildedrose/ItemUpdater.java
@@ -0,0 +1,74 @@
+package code.kata.gildedrose;
+
+public class ItemUpdater {
+	public Item item;
+
+	public ItemUpdater(Item item) {
+		this.item = item;
+	}
+
+	public void update() {
+		if (item.getName().equals("Aged Brie")) {
+			if (item.getQuality() < 50) {
+				item.setQuality(item.getQuality() + 1);
+	
+				if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+					if (item.getSellIn() < 11) {
+						if (item.getQuality() < 50) {
+							item.setQuality(item.getQuality() + 1);
+						}
+					}
+	
+					if (item.getSellIn() < 6) {
+						if (item.getQuality() < 50) {
+							item.setQuality(item.getQuality() + 1);
+						}
+					}
+				}
+			}
+		} else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+			if (item.getQuality() < 50) {
+				item.setQuality(item.getQuality() + 1);
+	
+				if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+					if (item.getSellIn() < 11) {
+						if (item.getQuality() < 50) {
+							item.setQuality(item.getQuality() + 1);
+						}
+					}
+	
+					if (item.getSellIn() < 6) {
+						if (item.getQuality() < 50) {
+							item.setQuality(item.getQuality() + 1);
+						}
+					}
+				}
+			}
+		} else if (item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+		} else {
+			if (item.getQuality() > 0) {
+				item.setQuality(item.getQuality() - 1);
+			}
+		}
+	
+		if (item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+		} else {
+			item.setSellIn(item.getSellIn() - 1);
+		}
+	
+		if (item.getSellIn() < 0) {
+			if (item.getName().equals("Aged Brie")) {
+				if (item.getQuality() < 50) {
+					item.setQuality(item.getQuality() + 1);
+				}
+			} else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+				item.setQuality(item.getQuality() - item.getQuality());
+			} else if (item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+			} else {
+				if (item.getQuality() > 0) {
+					item.setQuality(item.getQuality() - 1);
+				}
+			}
+		}
+	}
+}
